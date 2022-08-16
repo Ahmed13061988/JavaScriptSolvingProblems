84962b9360c426b6e11c26341f34d9c89d6d24e4
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-16 10:20:23 -0600

    solving the problem

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 34;
-const calcAge = birthYear => 2022 - birthYear;
+// const x = 34;
+// const calcAge = birthYear => 2022 - birthYear;
 //console.log(calcAge(1988));
 
 // We work for a company building a smart home thermometer. Our most recent task is this: " Given an array of tempretures of one day, calculate the tempretures amplitude. Keep in mind that sometimes there might be sensor error."
 
-const tempretures = [3, -2, -6 - 1, 'error', 9, 13, 17, 15, 14, 9];
+const tempretures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9];
 
 //Understanding the problem
 //1 - What is tempreture amplitude? Is the difference betweeen highest and lowest temp
@@ -20,3 +20,18 @@ const tempretures = [3, -2, -6 - 1, 'error', 9, 13, 17, 15, 14, 9];
 //3- find the minimum value in temp array
 //4- subtract min from max
 //5- return the results
+
+const calcTempAmplitude = function (tempretures) {
+  let max = tempretures[0];
+  let min = tempretures[0];
+  for (let i = 0; i < tempretures.length; i++) {
+    if (typeof tempretures[i] !== 'number') continue;
+    if (tempretures[i] > max) max = tempretures[i];
+    if (tempretures[i] < min) min = tempretures[i];
+  }
+  //console.log(max);
+  console.log(min);
+  const amplitude = max - min;
+  console.log(amplitude);
+};
+calcTempAmplitude(tempretures);
